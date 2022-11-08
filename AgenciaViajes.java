2d185d6192fc4396ff9ed23f62a5e0d1c568dd7e
protoboy <113700916+DincaAlex@users.noreply.github.com>
2022-11-08 14:42:09 -0500

    renamed "scanner" to "scan" and done some formatting

--- AgenciaViajes.java
+++ AgenciaViajes.java
@@ -12,8 +12,8 @@ public class AgenciaViajes {
         int opc;
         boolean salir = false;
         System.out.println("Ingrese el tipo de usuario: Administrador (1) o Cliente (2)");
-        Scanner scanner = new Scanner(System.in);
-        int user = scanner.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int user = scan.nextInt();
         Persistance p = new JSONConfigFile();
         p.leerConfig(config);
         while (!salir) {
@@ -22,21 +22,21 @@ public class AgenciaViajes {
                 switch (opc) {
                     case 1: 
                         System.out.println("Ingrese el correo con el que se registrara: ");
-                        String correo = scanner.next();
+                        String correo = scan.next();
                         System.out.println("Ingrese sus nombre: ");
-                        String nombres = scanner.next();
+                        String nombres = scan.next();
                         System.out.println("Ingrese sus apellido:");
-                        String apellidos = scanner.next();
+                        String apellidos = scan.next();
                         System.out.println("Ingrese una contrasena:");
-                        String contrasena = scanner.next();
+                        String contrasena = scan.next();
                         Admin admin= new Admin(correo, nombres, apellidos, contrasena);
                         config.registrarAdmin(admin);
                         break;
                     case 2:
                         System.out.println("Ingrese su correo:");
-                        String correoA = scanner.next();
+                        String correoA = scan.next();
                         System.out.println("Ingrese su contrasena: ");
-                        String contrasenaA = scanner.next();
+                        String contrasenaA = scan.next();
                         if(config.confirmarIngresoAdmin(correoA, contrasenaA))
                         {
                             System.out.println("Bienvenido.");
@@ -55,13 +55,13 @@ public class AgenciaViajes {
                 switch (opc) {
                     case 1:
                         System.out.println("Ingrese el correo con el que se registrara: ");
-                        String correo = scanner.next();
+                        String correo = scan.next();
                         System.out.println("Ingrese sus nombre: ");
-                        String nombres = scanner.next();
+                        String nombres = scan.next();
                         System.out.println("Ingrese sus apellido:");
-                        String apellidos = scanner.next();
+                        String apellidos = scan.next();
                         System.out.println("Ingrese una contrasena:");
-                        String contrasena = scanner.next();
+                        String contrasena = scan.next();
                         Cliente cliente = new Cliente(correo, nombres, apellidos, contrasena);
                         config.registrarCliente(cliente);
                         break;
@@ -85,30 +85,28 @@ public class AgenciaViajes {
         System.out.println("1. Registrar administrador");
         System.out.println("2. Ingresar como administrador");
         System.out.println("3. Salir");
-        Scanner b = new Scanner(System.in);
-        int opc = b.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int opc = scan.nextInt();
         return opc;
     }
-
-    
 
     public static int menuCliente()
     {
         System.out.println("1. Registrar cliente");
         System.out.println("2. Ingresar como cliente");
         System.out.println("3. Salir");
-        Scanner b = new Scanner(System.in);
-        int opc = b.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int opc = scan.nextInt();
         return opc;
     }
 
     public static int menuOpcAdm()
     {
         System.out.println("1. Agregar Rutas");
-        System.out.println("2. Agregar Transporte");
+        System.out.println("2. Agregar ruta.Transporte");
         System.out.println("3. Salir");
-        Scanner c= new Scanner(System.in);
-        int opc= c.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int opc = scan.nextInt();
         return opc;
     }
 
