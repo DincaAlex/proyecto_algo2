a02236a11251e7dcbf25e88d49dedeb91eea6d42
protoboy <113700916+DincaAlex@users.noreply.github.com>
2022-12-13 22:50:20 -0500

    moved "agregarCuarto" and "generarCuartosEnMasse" from "AgenciaViajes" to "ConfigCuartos"

--- processes/ConfigCuartos.java
+++ processes/ConfigCuartos.java
@@ -2,12 +2,14 @@ package processes;
 
 import Persistence.JSONConfigFileHoteles;
 import entities.Hotel.Cuarto;
+import entities.Hotel.Hotel;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ConfigCuartos implements Config<Cuarto> {
     private final HashMap<String, Cuarto> cuartos;
@@ -61,7 +63,7 @@ public class ConfigCuartos implements Config<Cuarto> {
         }
     }
 
-    public void autoGenerarCuartos(String nombre, String ciudad, int estrellas, int numCuartos, int pisos) {
+    public void autoGenerarCuartos (String nombre, String ciudad, int estrellas, int numCuartos, int pisos) {
         boolean ocupado = false;
         ConfigCuartos configCuartos = new ConfigCuartos();
         ConfigHoteles configHoteles = new ConfigHoteles();
@@ -79,7 +81,7 @@ public class ConfigCuartos implements Config<Cuarto> {
         p.guardarConfig(configHoteles, configCuartos);
     }
 
-    public int contarCuartosHotel(String nombre) {
+    public int contarCuartosHotel (String nombre) {
         int cant = 0;
         Enumeration<Cuarto> enumC = Collections.enumeration(this.cuartos.values());
         while (enumC.hasMoreElements()) {
@@ -88,5 +90,77 @@ public class ConfigCuartos implements Config<Cuarto> {
                 cant++;
         }
         return cant;
+    }
+
+    public void agregarCuarto () {
+        Scanner scan = new Scanner(System.in);
+        ConfigHoteles configHoteles = new ConfigHoteles();
+        ConfigCuartos configCuartos = new ConfigCuartos();
+        JSONConfigFileHoteles persistenceHoteles = new JSONConfigFileHoteles();
+        persistenceHoteles.leerConfig(configHoteles, configCuartos);
+        if (configHoteles.noHayHoteles()) {
+            System.out.println("No hay hoteles registrados.");
+        }
+        else {
+            configHoteles.mostrarHoteles();
+            System.out.println("Escoge el nombre del hotel: ");
+            String nombreH = scan.nextLine();
+            Hotel hotel = configHoteles.recuperarHotel(nombreH);
+            String ciudad = hotel.mostrarCiudad();
+            int estrellas = hotel.mostrarEstrellas();
+            System.out.println("Ingrese el numero del cuarto: ");
+            int numero = Integer.parseInt(scan.next());
+            System.out.println("Ingrese el piso del cuarto: ");
+            int piso = Integer.parseInt(scan.next());
+            System.out.println("Ingrese si el cuarto esta ocupado:");
+            boolean ocupado = Boolean.parseBoolean(scan.next());
+            int idNum = piso*100+numero;
+            String id = String.valueOf(idNum);
+            Cuarto cuarto = new Cuarto(nombreH, ciudad, estrellas, numero, piso, ocupado, "", id);
+            configCuartos.registrar(cuarto);
+            persistenceHoteles.guardarConfig(configHoteles, configCuartos);
+        }
+    }
+
+    public void generarCuartosEnMasse () {
+        Scanner scan = new Scanner(System.in);
+        ConfigHoteles configHoteles = new ConfigHoteles();
+        ConfigCuartos configCuartos = new ConfigCuartos();
+        JSONConfigFileHoteles persistenceHoteles = new JSONConfigFileHoteles();
+        persistenceHoteles.leerConfig(configHoteles, configCuartos);
+        if (configHoteles.noHayHoteles()) {
+            System.out.println("No hay hoteles registrados.");
+        }
+        else {
+            String ciudad;
+            int estrellas;
+            String nombreHotel;
+            String retry;
+            do {
+                retry = "N";
+                System.out.println("Lista de hoteles registrados:");
+                configHoteles.mostrarHoteles();
+                System.out.println("Elige el nombre del hotel: ");
+                nombreHotel = scan.nextLine();
+                Hotel hotel = configHoteles.recuperarHotel(nombreHotel);
+                ciudad = hotel.mostrarCiudad();
+                estrellas = hotel.mostrarEstrellas();
+
+                if (estrellas==-1) { // error para el caso en que lo ingresado no se encuentra
+                    System.out.println("Desea intentar de nuevo? [S/N]: ");
+                    retry = scan.next();
+                    if (retry.equalsIgnoreCase("n"))
+                        break;
+                }
+                else {
+                    System.out.println("Numero de pisos: ");
+                    int numP = scan.nextInt();
+                    System.out.println("Numero de cuartos por piso: ");
+                    int numC = scan.nextInt();
+                    configCuartos.autoGenerarCuartos(nombreHotel, ciudad, estrellas, numC, numP);
+                    persistenceHoteles.guardarConfig(configHoteles, configCuartos);
+                }
+            } while (retry.equalsIgnoreCase("s"));
+        }
     }
 }
