840cdeda19310791d614abe0e613776ff5de0752
protoboy <113700916+DincaAlex@users.noreply.github.com>
2022-12-13 22:41:58 -0500

    updated "Hoteles.json" and "Viajes.json"

--- processes/ConfigAdmins.java
+++ processes/ConfigAdmins.java
@@ -1,5 +1,6 @@
 package processes;
 
+import Persistence.JSONConfigFileUsuarios;
 import entities.Usuario.Admin;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -7,16 +8,17 @@ import org.json.simple.JSONObject;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ConfigAdmins implements Config<Admin> {
-    private HashMap<String, Admin> admins;
+    private final HashMap<String, Admin> admins;
 
     public ConfigAdmins () {
         this.admins = new HashMap<String, Admin>();
     }
 
     public void registrar (Admin args) {
-        if(this.admins.containsKey((Object)args.mostrarCorreo())){
+        if(this.admins.containsKey(args.mostrarCorreo())){
             System.out.println("Correo en uso actualmente, no se ha creado una nueva cuenta.");
             return;
         }
@@ -51,5 +53,72 @@ public class ConfigAdmins implements Config<Admin> {
             arrayAdmins.add(obj);
         }
         return arrayAdmins;
+    }
+
+    public void registrarAdmin () {
+        Scanner scan = new Scanner(System.in);
+        ConfigAdmins configAdmins = new ConfigAdmins();
+        ConfigClientes configClientes = new ConfigClientes();
+        JSONConfigFileUsuarios persistenceUsuarios = new JSONConfigFileUsuarios();
+        persistenceUsuarios.leerConfig(configAdmins, configClientes);
+        String retryAnswer;
+        do {
+            retryAnswer = "n";
+            System.out.println("Ingrese el correo con el que se registrara: ");
+            String correo = scan.next();
+            System.out.println("Ingrese su nombre: ");
+            String nombres = scan.next();
+            System.out.println("Ingrese su apellido:");
+            String apellidos = scan.next();
+            System.out.println("Ingrese una contrasena:");
+            String contrasena = scan.next();
+            if (configAdmins.confirmarIngresoAdmin(correo, contrasena)) {
+                System.out.println("El correo ya esta registrado. Desea intentar de nuevo? [S/N]: ");
+                retryAnswer = scan.next();
+                if (retryAnswer.equalsIgnoreCase("n"))
+                    break;
+            }
+            else {
+                Admin admin = new Admin(correo, nombres, apellidos, contrasena);
+                configAdmins.registrar(admin);
+                persistenceUsuarios.guardarConfig(configAdmins, configClientes);
+            }
+        } while (retryAnswer.equalsIgnoreCase("s"));
+    }
+
+    public boolean ingresarAdmin() {
+        Scanner scan = new Scanner(System.in);
+        ConfigAdmins configAdmins = new ConfigAdmins();
+        ConfigClientes configClientes = new ConfigClientes();
+        JSONConfigFileUsuarios persistenceUsuarios = new JSONConfigFileUsuarios();
+        persistenceUsuarios.leerConfig(configAdmins, configClientes);
+        String retryAnswer;
+        int retryTimes = 0;
+        boolean entradaExitosa = false;
+        do {
+            retryAnswer = "n";
+            System.out.println("Ingrese su correo:");
+            String correoA = scan.next();
+            System.out.println("Ingrese su contrasena: ");
+            String contrasenaA = scan.next();
+            if (!configAdmins.confirmarIngresoAdmin(correoA, contrasenaA)) {
+                System.out.println("No se encontró el usuario. Desea intentar de nuevo? [S/N]: ");
+                retryAnswer = scan.next();
+                if (retryAnswer.equalsIgnoreCase("n"))
+                    break;
+                if (retryTimes>=3) {
+                    System.out.println("Excedió el número máximo de intentos.");
+                    retryAnswer = "n";
+                    break;
+                }
+                if (retryAnswer.equalsIgnoreCase("s"))
+                    retryTimes++;
+            }
+            else {
+                entradaExitosa = true;
+                System.out.println("Bienvenido.\n");
+            }
+        } while (retryAnswer.equalsIgnoreCase("s"));
+        return entradaExitosa;
     }
 }
