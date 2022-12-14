8214fa9168e9e561c9eaabdf7f4fe29a0e3ecdf4
protoboy <113700916+DincaAlex@users.noreply.github.com>
2022-12-14 16:59:41 -0500

    added comment explaining "registrar" method

--- processes/Config.java
+++ processes/Config.java
@@ -3,6 +3,6 @@ package processes;
 import org.json.simple.JSONArray;
 
 public interface Config<T> {
-    void registrar (T args);
+    void registrar (T args); // sobreescribe el hashmap desde el json
     JSONArray ToJSON();
 }
