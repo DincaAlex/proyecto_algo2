08069f817bb2c9cb76363694c13b672f1606d7c7
protoboy <113700916+DincaAlex@users.noreply.github.com>
2022-12-13 22:45:57 -0500

    moved "agregarRutas" from "AgenciaViajes" to "ConfigRutas"

--- processes/ConfigRutas.java
+++ processes/ConfigRutas.java
@@ -1,5 +1,6 @@
 package processes;
 
+import Persistence.JSONConfigFileViajes;
 import entities.Viajes.Ruta;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -7,6 +8,7 @@ import org.json.simple.JSONObject;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ConfigRutas implements Config<Ruta> {
     private HashMap<String, Ruta> rutas;
@@ -15,7 +17,7 @@ public class ConfigRutas implements Config<Ruta> {
         this.rutas = new HashMap<String, Ruta>();
     }
 
-    public void registrar(Ruta args) {
+    public void registrar (Ruta args) {
         if(this.rutas.containsKey((Object)args.mostrarID())) {
             System.out.println("La ruta ya esta registrada.");
             return;
@@ -23,7 +25,7 @@ public class ConfigRutas implements Config<Ruta> {
         this.rutas.put(args.mostrarID(), args);
     }
 
-    public JSONArray ToJSON() {
+    public JSONArray ToJSON () {
         JSONArray arrayRutas = new JSONArray();
         Enumeration<Ruta> enumR = Collections.enumeration(this.rutas.values());
         while (enumR.hasMoreElements()) {
@@ -36,5 +38,25 @@ public class ConfigRutas implements Config<Ruta> {
             arrayRutas.add(obj);
         }
         return arrayRutas;
+    }
+
+    public void agregarRuta () {
+        Scanner scan = new Scanner(System.in);
+        ConfigRutas configRutas = new ConfigRutas();
+        ConfigTransportes configTransportes = new ConfigTransportes();
+        JSONConfigFileViajes persistenceViajes = new JSONConfigFileViajes();
+        persistenceViajes.leerConfig(configRutas, configTransportes);
+
+        System.out.println("Ingrese el ID de la ruta: ");
+        String ID = scan.next();
+        System.out.println("Ingrese la ciudad de partida: ");
+        String ciudadPartida = scan.next();
+        System.out.println("Ingrese la ciudad de destino:");
+        String ciudadDestino = scan.next();
+        System.out.println("Ingrese el tipo de transporte:");
+        String transporte = scan.next();
+        Ruta ruta = new Ruta(ID, ciudadPartida, ciudadDestino, transporte);
+        configRutas.registrar(ruta);
+        persistenceViajes.guardarConfig(configRutas, configTransportes);
     }
 }
