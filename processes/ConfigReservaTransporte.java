06af965d8e1fa7305214af31776df0d50630c966
protoboy <113700916+DincaAlex@users.noreply.github.com>
2022-12-15 21:45:54 -0500

    added "eliminar" method in "ConfigReservaTransporte"

--- processes/ConfigReservaTransporte.java
+++ processes/ConfigReservaTransporte.java
@@ -3,6 +3,7 @@ package processes;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Scanner;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -29,10 +30,10 @@ public class ConfigReservaTransporte implements Config<ReservaTransporte>{
         while(rt.hasMoreElements()) {
             ReservaTransporte rTrans = rt.nextElement();
             JSONObject obj = new JSONObject();
+            obj.put("IDReserva", rTrans.mostrarIDReserva());
             obj.put("IDCliente", rTrans.mostrarIDCliente());
             obj.put("IDRuta", rTrans.mostrarIDRuta());
             obj.put("IDTransporte", rTrans.mostrarIDTransporte());
-            obj.put("IDReserva", rTrans.mostrarIDReserva());
             arrayReservaTransporte.add(obj);
         }
         return arrayReservaTransporte;
@@ -54,7 +55,27 @@ public class ConfigReservaTransporte implements Config<ReservaTransporte>{
         guardar();
     }
 
-    public boolean mostrarReservasHotelCliente (String UUID) {
+    public void reservaTransporte() {
+    }
+
+    private void eliminar (String ID) {
+        actualizar();
+        Enumeration<ReservaTransporte> enumH = Collections.enumeration(reservasTransporte.values());
+
+        while (enumH.hasMoreElements()) {
+            ReservaTransporte reservaTransporte = enumH.nextElement();
+            if (ID.equals(reservaTransporte.mostrarIDReserva())) {
+                reservasTransporte.remove(reservaTransporte.mostrarIDRuta(), reservaTransporte);
+                reservasTransporte.remove(reservaTransporte.mostrarIDCliente(), reservaTransporte);
+                reservasTransporte.remove(reservaTransporte.mostrarIDTransporte(), reservaTransporte);
+                reservasTransporte.remove(reservaTransporte.mostrarIDReserva(), reservaTransporte);
+                System.out.println("Reserva de hotel eliminada exitosamente");
+                break;
+            }
+        }
+    }
+
+    public boolean mostrarReservasTransporteCliente (String UUID) {
         actualizar();
         Enumeration<ReservaTransporte> enu = Collections.enumeration(reservasTransporte.values());
 
@@ -77,5 +98,52 @@ public class ConfigReservaTransporte implements Config<ReservaTransporte>{
             }
         }
         return vacio;
+    }
+
+    public boolean mostrarReservasTransporte () {
+        actualizar();
+        Enumeration<ReservaTransporte> enu = Collections.enumeration(reservasTransporte.values());
+
+        boolean vacio = false;
+        int i = 1;
+        if (reservasTransporte.isEmpty()) {
+            System.out.println("No hay reservas de transportes");
+            vacio = true;
+        }
+        else {
+            while (enu.hasMoreElements()) {
+                ReservaTransporte reservaTransporte = enu.nextElement();
+                System.out.println(i + ". ID: " + reservaTransporte.mostrarIDReserva());
+                System.out.println(i + ". Cliente: " + reservaTransporte.mostrarIDCliente());
+                System.out.println(i + ". Ruta: " + reservaTransporte.mostrarIDRuta());
+                System.out.println(i + ". Transporte: " + reservaTransporte.mostrarIDTransporte() + "\n");
+                }
+                i++;
+            }
+        return vacio;
+    }
+
+    public void eliminarReservaTransporte () {
+        Scanner scan = new Scanner(System.in);
+        actualizar();
+
+        if (!mostrarReservasTransporte()) {
+            System.out.println("Ingrese el ID de la reserva: ");
+            String ID = scan.nextLine();
+            eliminar(ID);
+            guardar();
+        }
+    }
+
+    public void eliminarReservaTransporteCliente (String UUID) {
+        Scanner scan = new Scanner(System.in);
+        actualizar();
+
+        if (!mostrarReservasTransporteCliente(UUID)) {
+            System.out.println("Ingrese el ID de la reserva: ");
+            String ID = scan.nextLine();
+            eliminar(ID);
+            guardar();
+        }
     }
 }
